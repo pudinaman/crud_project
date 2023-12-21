349d2e3a2a080d7f0477cbbfbdff619626dc9029
ayush <ayush.raj2019@outlook.com>
2023-12-21 20:13:04 +0530

    added_request_method

--- server.js
+++ server.js
@@ -3,10 +3,26 @@ const http =require("http");
 const PORT =process.env.PORT || 5001;
 
 const server = http.createServer((req,res)=>{
-  res.statusCode=200;
-  res.setHeader("Content-Type","application/json");
-  res.write(JSON.stringify({message:"hello Ayush, to node js "}));
-  res.end();
+  switch(req.method){
+    case "GET":
+        getReq(req,res);
+        break;
+    case "POST":
+        postReq(req,res);
+        break;
+    case "PUT":
+        putReq(req,res);
+        break;
+    case "DELETE":
+        deleteReq(req,res);
+        break;
+    default:
+        res.statusCode=404;
+        res.setHeader("Content-Type","application/json");
+        res.write(JSON.stringify({title:"Not Found",message:"Route not found"}));
+        res.end();
+       
+  }
 });
 
 server.listen(PORT,()=>{
